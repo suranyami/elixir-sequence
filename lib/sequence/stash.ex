bf897465e3d879d27730da44b707077c468ff4d3
David Parry <david.parry@suranyami.com>
2014-03-20 21:37:17 +1100

    first import

---
+++ lib/sequence/stash.ex
@@ -0,0 +1,27 @@
+defmodule Sequence.Stash do
+  use GenServer.Behaviour
+  
+  def start_link(current_number) do
+    :gen_server.start_link(__MODULE__, current_number, [])
+  end
+
+  def save_value(pid, value) do
+    :gen_server.cast pid, {:save_value, value}
+  end
+
+  def get_value(pid) do
+    :gen_server.call pid, :get_value
+  end
+
+  def init(current_number) when is_number(current_number) do
+    {:ok, current_number}
+  end
+
+  def handle_call(:get_value, _from, current_value) do
+    {:reply, current_value, current_value}
+  end
+
+  def handle_cast({:save_value, value}, _current_value) do
+    {:noreply, value}
+  end
+end
